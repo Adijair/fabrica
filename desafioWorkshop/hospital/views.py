c3e2500f22b1d779889a0e897470edcd1ea3934b
--replace-all <adijairp05@gmail.com>
2023-04-01 22:19:10 -0300

    feito

---
+++ desafioWorkshop/hospital/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import Pessoa
+
+
+def home(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'index.html', {"pessoas": pessoas})
+
+
+def salvar(request):
+    request_nome = request.POST.get('nome')
+    Pessoa.objects.create(nome=request_nome)
+
+    pessoas = Pessoa.objects.all()
+    return render(request, "index.html", {'pessoas': pessoas})
+
+
+def editar(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    return render(request, 'update.html', {'pessoa': pessoa})
+
+
+def update(request, id):
+    request_nome_novo = request.POST.get('nome')
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.nome = request_nome_novo
+    pessoa.save()
+    return redirect(home)
+
+
+def delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    pessoa.delete()
+    return redirect(home)
